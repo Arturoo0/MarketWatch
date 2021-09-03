45cc3d66d699c8333978c590ec740955be847603
Arturoo0 <arturoportelles@gmail.com>
2021-09-03 16:34:13 -0400

    Add init frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Authentication from "./pages/Authentication";
+
+function App() {
+  return (
+    <div className="App">
+      <Authentication />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/LoginForm.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const LoginForm = () => {
+    return (
+        <div>
+            login form
+        </div>
+    );
+};
+
+export default LoginForm;
