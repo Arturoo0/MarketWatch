5b257ea9e624ea14cc3ed527565256f5959529b5
Bruception <bberr022@fiu.edu>
2021-09-11 18:23:46 -0400

    Add core initialization

--- backend/app.js
+++ backend/app.js
@@ -8,38 +8,53 @@ const auth = require('./routes/auth.js');
 const logger = require('./utils/logger');
 const marketData = require('./routes/marketData.js');
 const loggingMiddleware = require('./middleware/logging');
+const FinnHubClient = require('./services/finnhub-client');
 
-const app = express();
-const port = 3000;
-
-app.use(cors({
-  credentials: true,
-  origin: 'http://localhost:3001'
-}));
-app.use(cookieParser());
-app.use(express.urlencoded({
-  extended: true,
-}));
-app.use(express.json());
-
-app.use(loggingMiddleware);
-
-app.use('/auth', auth.authRouter);
-app.use('/market-data', marketData.marketDataRouter);
-
-try {
-  mongoose.connect(process.env.DB_URI);
-} catch (error){
-  if (error !== undefined) {
-    logger.error(error.message);
+async function initCore() {
+  logger.info('Connecting to mongo ...');
+  try {
+    mongoose.connect(process.env.DB_URI);
+  } catch (error) {
+    logger.error(error?.message);
+    process.exit(1);
   }
-  process.exit(1);
+
+  logger.info('Pre-fetching symbols ...');
+  try {
+    await FinnHubClient.fetchSymbols();
+  } catch (error) {
+    logger.error(error.message);
+    logger.error('Failed to pre-fetch, resorting to lazy load ...');
+  }
 }
 
-app.get('/', (req, res) => {
-  res.send('Server is running.');
-})
+async function initApp(port) {
+  await initCore();
 
-app.listen(port, () => {
-  logger.info(`Server listening at http://localhost:${port}`);
-});
+  const app = express();
+
+  app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3001'
+  }));
+  app.use(cookieParser());
+  app.use(express.urlencoded({
+    extended: true,
+  }));
+  app.use(express.json());
+  app.use(loggingMiddleware);
+  
+  app.use('/auth', auth.authRouter);
+  app.use('/market-data', marketData.marketDataRouter);
+
+  app.get('/', (req, res) => {
+    res.send('Server is running.');
+  })
+  
+  app.listen(port, () => {
+    logger.info(`Server listening at http://localhost:${port}`);
+  });
+}
+
+const PORT = 3000;
+initApp(PORT);
