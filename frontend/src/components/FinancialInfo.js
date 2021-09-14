5d9803d18adb4701d9856febf8206f4c3081c3ca
Arturoo0 <arturoportelles@gmail.com>
2021-09-14 17:51:03 -0400

    Render out basic initial financials'

--- frontend/src/components/FinancialInfo.js
+++ frontend/src/components/FinancialInfo.js
@@ -1,10 +1,48 @@
 
 import React from 'react';
 
-const FinancialInfo = () => {
+const financialLiStyling = {
+    display: 'flex',
+    justifyContent: 'space-between'
+}
+
+const liFinancialDescriptionStyling = {
+    color: '#444444'
+};
+
+const liFinancialValueStyling = {
+    fontWeight: '600'
+};
+
+const FinancialInfo = (props) => {
+    const generateFinancialsBlock = () => {
+        const financialOrder = [
+            'marketCapitalization',
+            '52WeekHigh',
+            '52WeekHighDate',
+            '52WeekLow',
+            '52WeekLowDate',
+            'currentDividendYieldTTM'
+        ];
+
+        const res = financialOrder.map(figure =>
+            <li style={financialLiStyling}>
+                <div style={liFinancialDescriptionStyling}>
+                    {figure}
+                </div>
+                <div style={liFinancialValueStyling}>
+                    {'hehe'}        
+                </div>
+            </li>
+        );
+        return res; 
+    }; 
+
     return (  
         <div>
-            testing
+            <ul>
+                {generateFinancialsBlock()}
+            </ul>
         </div>
     );
 }
