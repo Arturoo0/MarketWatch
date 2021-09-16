6ebad5865de1c12e16bbcba4be88bf8b816575a4
Arturoo0 <arturoportelles@gmail.com>
2021-09-16 12:45:23 -0400

    Fix sort state mutation

--- frontend/src/pages/Portfolios.js
+++ frontend/src/pages/Portfolios.js
@@ -42,8 +42,9 @@ const Portfolios = () => {
         if (portfolios.length === 0) {
             return <PageSpinner />;
         }
-        portfolios.sort((portfolioA, portfolioB) => portfolioB.lastEditedDate - portfolioA.lastEditedDate);
-        const portfolioList = portfolios.map((portfolio) => {
+        const currentPortfolios = [...portfolios];
+        currentPortfolios.sort((portfolioA, portfolioB) => portfolioB.lastEditedDate - portfolioA.lastEditedDate);
+        const portfolioList = currentPortfolios.map((portfolio) => {
             return (
                 <a key={portfolio._id} className='list-group-item list-group-item-action flex-column align-items-start'>
                     <div className='d-flex w-100 justify-content-between'>
