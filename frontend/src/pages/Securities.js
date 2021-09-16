677df0241ffa335609e04a664f539e8bfd4368a4
Bruception <bberr022@fiu.edu>
2021-09-16 15:49:15 -0400

    Add securities pagination

--- frontend/src/pages/Securities.js
+++ frontend/src/pages/Securities.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { Search, SymbolLookupCard } from '../components';
 import { get } from '../utils/baseRequest';
 import { useDebounce } from '../utils/utils';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Pagination } from 'react-bootstrap';
 
 const securitiesContainerStyle = {
     margin: '1rem',
@@ -19,9 +19,17 @@ const noMatchesTextStyle = {
     textAlign: 'center',
 }
 
+const MAX_PAGE_WINDOW_SIZE = 7;
+
 const Securities = () => {
+    const [previousSearchTerm, setPreviousSearchTerm] = useState('');
     const [searchTerm, setSearchTerm] = useState('');
-    const [matchedSymbols, setMatchedSymbols] = useState({});
+    const [pageNumber, setPageNumber] = useState(0);
+    const [searchQueryResult, setSearchQueryResult] = useState({
+        pages: 0,
+        symbols: [],
+        total: 0,
+    });
 
     const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
@@ -29,17 +37,22 @@ const Securities = () => {
         async function searchForQuery() {
             const searchQuery = {
                 keywords: debouncedSearchTerm,
+                offset: Math.max(pageNumber, 0),
             };
             const queryString = new URLSearchParams(searchQuery).toString();
             const response = await get(`/market-data/us-ex-symbols?${queryString}`);
-            const matchedSymbols = response.data;
-            setMatchedSymbols(matchedSymbols);
+            const queryResult = response.data;
+            setSearchQueryResult(queryResult);
+            setPreviousSearchTerm(debouncedSearchTerm);
+            if (previousSearchTerm !== debouncedSearchTerm) {
+                setPageNumber(0);
+            }
         }
         searchForQuery();
-    }, [debouncedSearchTerm]);
+    }, [pageNumber, previousSearchTerm, debouncedSearchTerm]);
 
     const renderMatchedSymbols = () => {
-        const renderedCards = matchedSymbols?.US_EX_SYMBOLS?.map((symbol) => {
+        const renderedCards = searchQueryResult?.symbols?.map((symbol) => {
             const cardData = {
                 symbol: symbol.displaySymbol,
                 description: symbol.description,
@@ -52,10 +65,60 @@ const Securities = () => {
             : <div style={noMatchesTextStyle}>No securities match this query.</div>
     }
 
+    const paginationItemOnClick = (pageIndex) => {
+        return () => {
+            const normalizedPageIndex = Math.min(
+                Math.max(pageIndex, 0),
+                searchQueryResult.pages - 1
+            );
+            setPageNumber(normalizedPageIndex);
+        }
+    }
+
+    const renderPagination = () => {
+        const pageWindowSize = Math.min(MAX_PAGE_WINDOW_SIZE, searchQueryResult.pages);
+        const offset = Math.floor(pageWindowSize / 2);
+
+        let pageNumbersLeft = Math.max(pageNumber - offset, 0);
+        let pageNumbersRight = Math.min(pageNumber + offset, searchQueryResult.pages - 1);
+        const totalPages = pageNumbersRight - pageNumbersLeft + 1;
+
+        if (totalPages < pageWindowSize) {
+            const difference = pageWindowSize - totalPages;
+            if (pageNumber - pageNumbersLeft < offset) {
+                pageNumbersRight += difference;
+            } else {
+                pageNumbersLeft -= difference;
+            }
+        }
+
+        const pageItems = [];
+        for (let pageId = pageNumbersLeft; pageId <= pageNumbersRight; ++pageId) {
+            const pageItem = (
+                <Pagination.Item
+                    active={pageId === pageNumber}
+                    key={pageId}
+                    onClick={paginationItemOnClick(pageId)}
+                >
+                    {pageId + 1}
+                </Pagination.Item>
+            );
+            pageItems.push(pageItem);
+        }
+        return pageItems;
+    }
+
     return (
         <div style={securitiesContainerStyle}>
             <h2>Securities</h2>
             <Search onChangeHandle={(text) => {setSearchTerm(text)}}/>
+            <Pagination>
+                <Pagination.First onClick={paginationItemOnClick(0)} />
+                <Pagination.Prev onClick={paginationItemOnClick(pageNumber - 1)}/>
+                {renderPagination()}
+                <Pagination.Next onClick={paginationItemOnClick(pageNumber + 1)}/>
+                <Pagination.Last onClick={paginationItemOnClick(searchQueryResult.pages - 1)}/>
+            </Pagination>
             <Row style={symbolContainerStyle}>
                 {renderMatchedSymbols()}
             </Row>
